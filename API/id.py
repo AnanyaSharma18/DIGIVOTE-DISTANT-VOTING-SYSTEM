d4dd9f3c59837be76c7181e232848afc619cf945
Khushi patel <125114444+khushiipatel04@users.noreply.github.com>
2025-03-29 12:21:07 +0530

    Update id.py

--- API/id.py
+++ API/id.py
@@ -1,12 +1,10 @@
-#Chances of collision is rare but not impossible. So, it is better to check in the database if the id is unique or not.
-#run this code and check in the database if the id is unique or not
-#If it is unique then you can use this id for the new user other create a new id.
 import uuid
 import datetime
 
 def generate_unique_id():
-     timestamp_part = datetime.datetime.now().strftime("%y%m%d%f")[-6:]  # Last 6 digits of microseconds
-     unique_part = uuid.uuid4().hex[:4].upper()  # First 4 characters of UUID
-     new_id = f"DV{timestamp_part}{unique_part}"
+    timestamp_part = datetime.datetime.now().strftime("%y%m%d%f")[-6:]  # Last 6 digits of microseconds
+    unique_part = uuid.uuid4().hex[:4].upper()  # First 4 characters of UUID
+    new_id = f"DV{timestamp_part}{unique_part}"
+    return new_id  # Return the generated ID
 
-print(generate_unique_id())
+print(generate_unique_id())  
